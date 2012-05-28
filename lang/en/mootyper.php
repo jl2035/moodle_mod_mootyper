96cc12707c9e380355498fc142825695fff8f7bf
Jaka Luthar <jaka.luthar@gmail.com>
2012-05-28 15:18:23 +0200

    28.05.2012 - Lang folder added

---
+++ lang/en/mootyper.php
@@ -0,0 +1,84 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+
+/**
+ * English strings for mootyper
+ *
+ * @package    mod
+ * @subpackage mootyper
+ * @copyright  2012 Jaka Luthar (jaka.luthar@gmail.com)
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['modulename'] = 'MooTyper';
+$string['modulenameplural'] = 'MooTypers';
+$string['modulename_help'] = 'Use the mootyper module for... | The mootyper module allows...';
+$string['mootyperfieldset'] = 'Custom example fieldset';
+$string['mootypername'] = 'Test title';
+$string['mootypername_help'] = 'This is the content of the help tooltip associated with the mootypername field. Markdown syntax is supported.';
+$string['mootyper'] = 'mootyper';
+$string['pluginadministration'] = 'MooTyper Administration';
+$string['pluginname'] = 'MooTyper';
+$string['fexercise'] = 'Exercise';
+$string['fsecurity'] = 'Security';
+$string['mootyperopentime'] = 'Open time';
+$string['mootyperclosetime'] = 'Close time';
+$string['requirepassword'] = 'Require password';
+$string['rtime'] = 'Time: ';
+$string['rprogress'] = 'Progress: ';
+$string['rprecision'] = 'Precision: ';
+$string['rhitspermin'] = 'Hits per minute: ';
+$string['rmistakes'] = 'Mistakes: ';
+$string['fcontinue'] = 'Continue';
+$string['configrequirepassword'] = 'Configure password';
+$string['ename'] = 'Exercise title';
+$string['etext'] = 'Text';
+$string['eremove'] = 'Remove';
+$string['etitle'] = 'MooTyper exercises';
+$string['eheading'] = 'Manage MooTyper exercises';
+$string['eaddnew'] = 'Add new exercise';
+$string['emanage'] = 'To manage exercises click here';
+$string['viewgrades'] = 'View all grades';
+$string['vmistakes'] = 'Mistakes';
+$string['student'] = 'Student';
+$string['timeinseconds'] = 'Time';
+$string['hitsperminute'] = 'Hits/minute';
+$string['fullhits'] = 'All hits';
+$string['precision'] = 'Precision';
+$string['nogrades'] = 'No grades yet...';
+$string['isexamtext'] = 'Exam';
+$string['flesson'] = 'Lesson';
+$string['sflesson'] = 'Lesson';
+$string['lsnname'] = 'lesson name';
+$string['requiredgoal'] = 'Required precision';
+$string['fmode'] = 'Mode';
+$string['fconfirm'] = 'Confirm';
+$string['notreadyyet'] = 'Not ready yet...please try again later.';
+$string['fsetup'] = 'Setup';
+$string['endlesson'] = 'End of the lesson';
+$string['examdone'] = 'Exam already taken.';
+$string['gviewmode'] = 'View';
+$string['byuser'] = 'by user';
+$string['bymootyper'] = 'by exercise';
+$string['allstring'] = 'All';
+$string['timetaken'] = 'Time taken';
+$string['fnewlesson'] = 'create new lesson';
+$string['fnewexercise'] = 'Exercise will be a part of';
+$string['average'] = 'Average';
