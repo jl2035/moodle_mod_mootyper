721e733da2966a21d8cfc32dc53892b050bed08e
Jaka Luthar <jaka.luthar@gmail.com>
2012-08-24 16:19:10 +0200

    1.5 - Some changes - 24.08.2012

--- index.php
+++ index.php
@@ -51,6 +51,8 @@ if (! $mootypers = get_all_instances_in_course('mootyper', $course)) {
     notice(get_string('nomootypers', 'mootyper'), new moodle_url('/course/view.php', array('id' => $course->id)));
 }
 
+$table = new html_table();
+
 if ($course->format == 'weeks') {
     $table->head  = array(get_string('week'), get_string('name'));
     $table->align = array('center', 'left');
