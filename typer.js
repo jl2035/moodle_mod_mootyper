be4340171cd9f7f9698bbb8b64610c24e5151943
Ruben Cancho <canchete@gmail.com>
2015-10-14 15:30:12 -0400

    Renamed trenutnaPos and trenutniChar

--- typer.js
+++ typer.js
@@ -1,10 +1,10 @@
 var startTime;
 var endTime;
 var napake;
-var trenutnaPos;
+var currentPos;
 var started = false;
 var ended = false;
-var trenutniChar;
+var currentChar;
 var fullText;
 var intervalID = -1;
 var interval2ID = -1;
@@ -79,7 +79,7 @@ function focusSet(e) {
 		return true;
 	}
 	else{
-		$('#tb1').val(fullText.substring(0, trenutnaPos));
+		$('#tb1').val(fullText.substring(0, currentPos));
 		return true;
 	}
 }
@@ -88,16 +88,16 @@ function doCheck() {
     var rpMootyperId = $('input[name="rpSityperId"]').val();
     var rpUser = $('input[name="rpUser"]').val();
     var rpAttId = $('input[name="rpAttId"]').val();
-    var juri =  app_url+"/mod/mootyper/atchk.php?status=2&attemptid="+rpAttId+"&mistakes="+napake+"&hits="+(trenutnaPos+napake);
+    var juri =  app_url+"/mod/mootyper/atchk.php?status=2&attemptid="+rpAttId+"&mistakes="+napake+"&hits="+(currentPos+napake);
 	$.get(juri, function( data ) { });
 }
 
 function doStart() {
 	startTime = new Date();
 	napake = 0;
-	trenutnaPos = 0;
+	currentPos = 0;
 	started = true;
-	trenutniChar = fullText[trenutnaPos];
+	currentChar = fullText[currentPos];
 	intervalID = setInterval('updTimeSpeed()', 1000);
     var rpMootyperId = $('input[name="rpSityperId"]').val();
     var rpUser = $('input[name="rpUser"]').val();
@@ -114,20 +114,20 @@ function keyPressed(e) {
 	if(!started)
 		doStart();
 	var keychar = getPressedChar(e);
-	if(keychar == trenutniChar || ((trenutniChar == '\n' || trenutniChar == '\r\n' || trenutniChar == '\n\r' || trenutniChar == '\r') && (keychar == ' ')))
+	if(keychar == currentChar || ((currentChar == '\n' || currentChar == '\r\n' || currentChar == '\n\r' || currentChar == '\r') && (keychar == ' ')))
 	{
-		if(trenutnaPos == fullText.length-1) {  //END
-			$('#tb1').val($('#tb1').val()+trenutniChar);
-			var elemOff = new keyboardElement(trenutniChar);
+		if(currentPos == fullText.length-1) {  //END
+			$('#tb1').val($('#tb1').val()+currentChar);
+			var elemOff = new keyboardElement(currentChar);
 			elemOff.turnOff();
 			doKonec();
 			return true;
 	    }
 	    
-	    if(trenutnaPos < fullText.length-1){
-			var nextChar = fullText[trenutnaPos+1];
+	    if(currentPos < fullText.length-1){
+			var nextChar = fullText[currentPos+1];
 			if(show_keyboard){
-				var thisE = new keyboardElement(trenutniChar);
+				var thisE = new keyboardElement(currentChar);
 				thisE.turnOff();
 				if(isCombined(nextChar) && (thisE.shift || thisE.alt || thisE.pow || thisE.uppercase_umlaut))
 					combinedCharWait = true;
@@ -139,9 +139,9 @@ function keyPressed(e) {
 				$("#form1").on("keyup", "#tb1", keyupFirst);
 			}
 		}
-		moveCursor(trenutnaPos+1);
-		trenutniChar = fullText[trenutnaPos+1];
-		trenutnaPos++;
+		moveCursor(currentPos+1);
+		currentChar = fullText[currentPos+1];
+		currentPos++;
 	    return true;	
 	}
 	else if(keychar == ' ')  //I don't remember why we're having this if
@@ -189,12 +189,12 @@ function initTextToEnter(ttext, tinprogress, tmistakes, thits, tstarttime, tatte
 		$('input[name="rpAttId"]').val(tattemptid);
 		startTime = new Date(tstarttime*1000);
 		napake = tmistakes;
-		trenutnaPos = (thits - tmistakes);   //!!!!!!!!!!!!!!!!!!!!!!!!!!!
-	    trenutniChar = fullText[trenutnaPos]; // current character (trenutni = current)
+		currentPos = (thits - tmistakes);   //!!!!!!!!!!!!!!!!!!!!!!!!!!!
+	    currentChar = fullText[currentPos]; // current character (trenutni = current)
 	    if(show_keyboard) {
-			var nextE = new keyboardElement(trenutniChar);
+			var nextE = new keyboardElement(currentChar);
 			nextE.turnOn();
-			if(isCombined(trenutniChar)) {
+			if(isCombined(currentChar)) {
 				$("#form1").off("keypress", "#tb1", keyPressed);
 				$("#form1").on("keyup", "#tb1", keyupCombined);
 			}
@@ -202,15 +202,15 @@ function initTextToEnter(ttext, tinprogress, tmistakes, thits, tstarttime, tatte
 	    started = true;
 	    intervalID = setInterval('updTimeSpeed()', 1000);
 	    interval2ID = setInterval('doCheck()', 3000);
-		for(var i=0; i<trenutnaPos; i++) {
+		for(var i=0; i<currentPos; i++) {
 			var tChar = ttext[i];
 			if(tChar == '\n')
 				tempStr += "<span id='crka"+i+"' class='txtZeleno'>&darr;</span><br>";
 			else
 				tempStr += "<span id='crka"+i+"' class='txtZeleno'>"+tChar+"</span>";
 		}
-		tempStr += "<span id='crka"+trenutnaPos+"' class='txtModro'>"+trenutniChar+"</span>";
-		for(var j=trenutnaPos+1; j<ttext.length; j++) {
+		tempStr += "<span id='crka"+currentPos+"' class='txtModro'>"+currentChar+"</span>";
+		for(var j=currentPos+1; j<ttext.length; j++) {
 			var tChar = ttext[j];
 			if(tChar == '\n')
 				tempStr += "<span id='crka"+j+"' class='txtRdece'>&darr;</span><br>";
@@ -248,13 +248,13 @@ function isDigit(aChar) {
 }
 
 function izracunajHitrost(sc) {
-	return (((trenutnaPos + napake) * 60) / sc);
+	return (((currentPos + napake) * 60) / sc);
 }
 
 function izracunajTocnost() {
-	if(trenutnaPos+napake == 0)
+	if(currentPos+napake == 0)
 		return 0;
-	return ((trenutnaPos * 100) / (trenutnaPos+napake));
+	return ((currentPos * 100) / (currentPos+napake));
 }
 
 function updTimeSpeed() {	
@@ -264,7 +264,7 @@ function updTimeSpeed() {
 	$('#jsTime').html(secs);
 	//$('#jsSpeed').html(izracunajHitrost(fullText, napake, secs).toFixed(2));
 	$('#jsMistakes').html(napake);
-	$('#jsProgress').html(trenutnaPos + "/" +fullText.length);
+	$('#jsProgress').html(currentPos + "/" +fullText.length);
 	$('#jsSpeed').html(izracunajHitrost(secs).toFixed(2));
 	$('#jsAcc').html(izracunajTocnost(fullText, napake).toFixed(2));
 }
