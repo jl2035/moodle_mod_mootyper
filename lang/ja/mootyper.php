20df6a81e357163f236e4f06b121a5ccddb93c0c
papillon <root@ws15.ypu.jp>
2016-01-25 17:10:33 +0900

    Add Japanese support (WIP)

---
+++ lang/ja/mootyper.php
@@ -0,0 +1,107 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+
+/**
+ * English strings for mootyper
+ *
+ * @package    mod
+ * @subpackage mootyper
+ * @copyright  2012 Jaka Luthar (jaka.luthar@gmail.com)
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['modulename'] = 'MooTyper';
+$string['modulenameplural'] = 'MooTypers';
+$string['modulename_help'] = 'The mootyper module allows us to learn how to type';
+$string['mootyperfieldset'] = 'Custom example fieldset';
+$string['mootypername'] = 'Test title';
+$string['mootypername_help'] = 'This is the content of the help tooltip associated with the mootypername field. Markdown syntax is supported.';
+$string['mootyper'] = 'mootyper';
+$string['pluginadministration'] = 'MooTyper Administration';
+$string['pluginname'] = 'MooTyper';
+$string['fexercise'] = 'Exercise';
+$string['fsecurity'] = 'Security';
+$string['fsettings'] = 'Settings';
+$string['mootyperopentime'] = 'Open time';
+$string['mootyperclosetime'] = 'Close time';
+$string['requirepassword'] = 'Require password';
+$string['rtime'] = 'Time: ';
+$string['rprogress'] = 'Progress: ';
+$string['rprecision'] = 'Precision: ';
+$string['rhitspermin'] = 'Hits per minute: ';
+$string['rmistakes'] = 'Mistakes: ';
+$string['fcontinue'] = 'Continue';
+$string['configrequirepassword'] = 'Configure password';
+$string['ename'] = 'Exercise title';
+$string['etext'] = 'Text';
+$string['eremove'] = 'Remove';
+$string['etitle'] = 'MooTyper exercises';
+$string['eheading'] = 'Manage MooTyper exercises';
+$string['eaddnew'] = 'Add new exercise / category';
+$string['emanage'] = 'To manage exercises and categories click here';
+$string['viewgrades'] = 'View all grades';
+$string['viewmygrades'] = 'View my grades';
+$string['vmistakes'] = 'Mistakes';
+$string['student'] = 'Student';
+$string['timeinseconds'] = 'Time';
+$string['hitsperminute'] = 'Hits/minute';
+$string['fullhits'] = 'All hits';
+$string['precision'] = 'Precision';
+$string['nogrades'] = 'No grades yet...';
+$string['isexamtext'] = 'Exam';
+$string['flesson'] = 'Lesson';
+$string['sflesson'] = 'Lesson';
+$string['lsnname'] = 'category name';
+$string['requiredgoal'] = 'Required precision';
+$string['fmode'] = 'Mode';
+$string['fconfirm'] = 'Confirm';
+$string['notreadyyet'] = 'Not ready yet...please try again later.';
+$string['fsetup'] = 'Setup';
+$string['endlesson'] = 'End of the category / lesson';
+$string['examdone'] = 'Exam already taken.';
+$string['gviewmode'] = 'View';
+$string['byuser'] = 'by user';
+$string['bymootyper'] = 'by exercise';
+$string['allstring'] = 'All';
+$string['timetaken'] = 'Time taken';
+$string['fnewlesson'] = 'create new lesson/category';
+$string['chere'] = 'Click here to start';
+$string['fnewexercise'] = 'Exercise will be a part of';
+$string['average'] = 'Average';
+$string['eeditlabel'] = 'Edit';
+$string['layout'] = 'Keyboard layout';
+$string['defaultlayout'] = 'Default keyboard layout';
+$string['showkeyboard'] = 'Show keyboard';
+$string['defaultprecision'] = 'Default typing precision %';
+$string['visibility'] = 'Category visible to';
+$string['editable'] = 'Can edit';
+$string['vaccess0'] = 'All teachers';
+$string['vaccess1'] = 'Teacher inside this course';
+$string['vaccess2'] = 'Only me';
+$string['eaccess0'] = 'Any teacher';
+$string['eaccess1'] = 'Teacher inside this course';
+$string['eaccess2'] = 'Only me';
+$string['reqfield'] = 'Required field';
+$string['excategory'] = 'Exercises category';
+$string['removeall'] = 'Remove all from ';
+$string['removeconfirm'] = 'You are about to completly remove ';
+$string['wpm'] = 'WPM';
+$string['csvexport'] = 'Export to .csv';
+$string['gradesfilename'] = 'grades.csv';
