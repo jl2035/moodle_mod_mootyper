0b3d4324bfaf8b948c2b7a64cc5b34f814178285
AL Rachels <drachels@drachels.com>
2016-03-13 12:29:58 -0500

    Update owngrades.php
    Added two missing table cells to fix broken border in Chrome.

--- owngrades.php
+++ owngrades.php
@@ -114,7 +114,7 @@ $htmlout .= '<div id="mainDiv">';
 		}
 		$avg = get_grades_avg($grds);
 		if(!$mootyper->isexam)
-			$htmlout .= '<tr style="border-top-style: solid;"><td><strong>'.get_string('average', 'mootyper').': </strong></td><td>'.$avg['mistakes'].'</td><td>'.$avg['timeinseconds'].' s</td><td>'.$avg['hitsperminute'].'</td><td>'.$avg['fullhits'].'</td><td>'.$avg['precisionfield'].'%</td></tr>';
+			$htmlout .= '<tr style="border-top-style: solid;"><td><strong>'.get_string('average', 'mootyper').': </strong></td><td>'.$avg['mistakes'].'</td><td>'.$avg['timeinseconds'].' s</td><td>'.$avg['hitsperminute'].'</td><td>'.$avg['fullhits'].'</td><td>'.$avg['precisionfield'].'%</td><td></td><td></td></tr>';
 		$htmlout .= '</table>';
 	}
 	else
