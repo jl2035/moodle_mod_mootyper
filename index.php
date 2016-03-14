22e1601252b85d0671f8603b2a25749da9c5e563
AL Rachels <drachels@drachels.com>
2016-03-14 09:59:08 -0500

    Update index.php
    Fix for add_to_log() has been deprecated.

--- index.php
+++ index.php
@@ -36,8 +36,6 @@ $course = $DB->get_record('course', array('id' => $id), '*', MUST_EXIST);
 
 require_course_login($course);
 
-add_to_log($course->id, 'mootyper', 'view all', 'index.php?id='.$course->id, '');
-
 $coursecontext = context_course::instance($course->id);
 
 $PAGE->set_url('/mod/mootyper/index.php', array('id' => $id));
@@ -85,4 +83,13 @@ foreach ($mootypers as $mootyper) {
 
 echo $OUTPUT->heading(get_string('modulenameplural', 'mootyper'), 2);
 echo html_writer::table($table);
+
+// Trigger course module instance list event.
+$params = array(
+    'context' => context_course::instance($course->id)
+);
+$event = \mod_mootyper\event\course_module_instance_list_viewed::create($params);
+$event->add_record_snapshot('course', $course);
+$event->trigger();
+
 echo $OUTPUT->footer();
