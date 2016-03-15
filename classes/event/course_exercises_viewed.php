71891b6ea78ee39a8df9270b06d7c0ee5a3d182b
AL Rachels <drachels@drachels.com>
2016-03-15 18:15:12 -0500

    Create course_exercises_viewed.php
    Added event for logging when someone views all the MooTyper exercises in a course.

---
+++ classes/event/course_exercises_viewed.php
@@ -0,0 +1,78 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * The mod_mootyper course exercises viewed event.
+ *
+ * @package    mod_mootyper
+ * @copyright  2016 AL Rachels drachels@drachels.com
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace mod_mootyper\event;
+
+defined('MOODLE_INTERNAL') || die();
+
+class course_exercises_viewed extends \core\event\base {
+
+    /**
+     * Init method.
+     *
+     * @return void
+     */
+    protected function init() {
+        $this->data['crud'] = 'r';
+        $this->data['edulevel'] = self::LEVEL_OTHER;
+        $this->data['objecttable'] = 'mootyper';
+    }
+
+	    /**
+     * Returns localised general event name.
+     *
+     * @return string
+     */
+    public static function get_name() {
+        return get_string('course_exercises_viewed', 'mod_mootyper');
+    }
+
+    /**
+     * Returns description of what happened.
+     *
+     * @return string
+     */
+    public function get_description() {
+        return "The user with id '$this->userid' viewed the exercises for mootyper activities in the course with id
+            '$this->contextinstanceid'";
+    }
+	
+    /**
+     * Returns relevant URL.
+     * @return \moodle_url
+     */
+    public function get_url() {
+        return new \moodle_url('/mod/mootyper/exercises.php', array('f' => $this->objectid));
+    }
+
+    /**
+     * Return the legacy event log data.
+     *
+     * @return array|null
+     */
+    protected function get_legacy_logdata() {
+        return array($this->courseid, 'mootyper', 'view mootyper', 'exercises.php?f=' . $this->objectid,
+            $this->objectid, $this->contextinstanceid);
+    }
+}
