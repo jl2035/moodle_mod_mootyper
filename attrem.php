396a1ad049ba0f5721931e977336ee819381ef6a
AL Rachels <drachels@drachels.com>
2016-03-15 10:20:53 -0500

    Update attrem.php
    Code and comments cleanup.

--- attrem.php
+++ attrem.php
@@ -15,21 +15,19 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * This is a one-line short description of the file
- *
- * You can have a rather longer description of the file as well,
- * if you like, and it can span multiple lines.
+ * This fils is used to remove a student attempt. Called from gview.php (View All Grades). 
  *
  * @package    mod
  * @subpackage mootyper
  * @copyright  2011 Jaka Luthar (jaka.luthar@gmail.com)
+ * @copyright  2016 onwards AL Rachels (drachels@drachels.com)
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
+
 require_once(dirname(dirname(dirname(__FILE__))).'/config.php');
-//require_once(dirname(dirname(dirname(__FILE__))).'/lib/dmllib.php');
+
 global $DB;
-//http://localhost/moodle/course/view.php?id=2
-//$DB->insert_record('mootyper_grades', $record, false);
+
 if(isset($_GET['g'])){
 	$gradeID = $_GET['g'];
 	$db_grade = $DB->get_record('mootyper_grades', array('id' => $gradeID));
@@ -40,14 +38,5 @@ $mID = $_GET['m_id'];
 $cID = $_GET['c_id'];
 $webDir = $CFG->wwwroot . '/mod/mootyper/gview.php?id='.$cID.'&n='.$mID;
 header('Location: '.$webDir);
-/*
-    $protocol = strpos(strtolower($_SERVER['SERVER_PROTOCOL']),'https') 
-                    === FALSE ? 'http' : 'https';
-    $host     = $_SERVER['HTTP_HOST'];
-    $script   = $_SERVER['SCRIPT_NAME'];
-    $params   = $_SERVER['QUERY_STRING'];
-    $currentUrl = $protocol . '://' . $host . $script . '?' . $params;
-    echo $currentUrl; 
- */
 
 ?>
