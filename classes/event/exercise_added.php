832a7f615e2bb743787261dc6a72776531c776f9
AL Rachels <drachels@drachels.com>
2016-03-16 09:03:53 -0500

    Update exercise_added.php
    Simplified log message.

--- classes/event/exercise_added.php
+++ classes/event/exercise_added.php
@@ -51,7 +51,7 @@ class exercise_added extends \core\event\base {
      * @return string
      */
     public function get_description() {
-        return "The user with id '$this->userid' added an exercise/category for mootyper activities in the course with id
+        return "The user with id '$this->userid' added a mootyper exercise/category while in the course with id
             '$this->contextinstanceid'";
     }
 
